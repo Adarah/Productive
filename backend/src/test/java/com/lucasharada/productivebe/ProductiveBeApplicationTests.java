fcb1c8d15a20914733b436cae3613582a538512e
Adarah <36046024+Adarah@users.noreply.github.com>
2022-02-02 20:10:58 -0300

    Add bazel CI (#1)
    * Add bazel CI
    
    * remove uneeded bazel setup action
    
    * fix failing tests
    
    * save CI error logs
    
    * run tests on java 17

--- backend/src/test/java/com/lucasharada/productivebe/ProductiveBeApplicationTests.java
+++ backend/src/test/java/com/lucasharada/productivebe/ProductiveBeApplicationTests.java
@@ -2,7 +2,10 @@ package com.lucasharada.productivebe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class ProductiveBeApplicationTests {
 
